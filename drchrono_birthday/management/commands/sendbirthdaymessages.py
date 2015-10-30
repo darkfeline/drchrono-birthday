968886c81da3c2eabaef3cabba1652f21e4fdf32
Allen Li <darkfeline@felesatra.moe>
2015-10-30 02:44:04 -0700

    Add GPL

--- drchrono_birthday/management/commands/sendbirthdaymessages.py
+++ drchrono_birthday/management/commands/sendbirthdaymessages.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2015  Allen Li
+#
+# This file is part of drchrono_birthday.
+#
+# drchrono_birthday is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# drchrono_birthday is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with drchrono_birthday.  If not, see <http://www.gnu.org/licenses/>.
+
 import datetime
 import smtplib
 from email.mime.text import MIMEText
